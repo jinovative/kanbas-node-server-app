68faeeae33c9e6323d4df68c9d2ccf709d0c998e
Jin <jinyung7428@gmail.com>
2024-04-03 20:11:52 -0400

    upload for mongoDB

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
-import CourseRoutes from "../kanbas-react-web-app/src/Kanbas/courses/routes.js";
-import ModuleRoutes from "../kanbas-react-web-app/src/Kanbas/Modules/routes.js";
+
+import UserRoutes from "./Users/routes.js";
 import cors from "cors";
 
+// import CourseRoutes from "../kanbas-react-web-app/src/Kanbas/Courses/routes.js";
+// import ModuleRoutes from "../kanbas-react-web-app/src/Kanbas/Modules/routes.js";
+
 const app = express();
-// const cors = require("cors");
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 app.use(cors());
 app.use(express.json());
 
-ModuleRoutes(app);
-CourseRoutes(app);
+UserRoutes(app);
+
+// ModuleRoutes(app);
+// CourseRoutes(app);
 Hello(app);
 Lab5(app);
 
