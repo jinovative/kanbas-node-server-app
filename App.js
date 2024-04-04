801d743c7165a09841e55ad2d85c1a000a731d4b
Jin <126914439+jinovative@users.noreply.github.com>
2024-04-04 09:19:12 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 import express from "express";
-import mongoose from "mongoose";
+
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 
@@ -10,7 +10,7 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import UserRoutes from "./Users/routes.js";
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 app.use(cors());
 app.use(express.json());
 
