c8122167960a53d1fa4658bbf80cdc754e782cde
Jin <126914439+jinovative@users.noreply.github.com>
2024-04-04 09:19:39 -0400

    Update App.js

--- App.js
+++ App.js
@@ -7,14 +7,14 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 
-import UserRoutes from "./Users/routes.js";
+
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
 
-UserRoutes(app);
+
 
 ModuleRoutes(app);
 CourseRoutes(app);
