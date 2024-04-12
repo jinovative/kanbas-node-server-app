e3434d342a9f3b9c7606015631caf87ca4c27fd3
Jin <126914439+jinovative@users.noreply.github.com>
2024-04-12 18:51:37 -0400

    re-update original

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
 import cors from "cors";
 import express from "express";
-
+import mongoose from "mongoose";
+import session from "express-session";
+import "dotenv/config";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
-
+import UserRoutes from "./Users/routes.js";
 
 const app = express();
 
+// const CONNECTION_STRING =
+//   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+// mongoose.connect(CONNECTION_STRING);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/kanbas", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+
+app.use(session(sessionOptions));
+
 app.use(express.json());
-
-
 
 ModuleRoutes(app);
 CourseRoutes(app);
+
+UserRoutes(app);
+
 Hello(app);
 Lab5(app);
 
